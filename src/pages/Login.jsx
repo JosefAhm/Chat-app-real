069bb344793946025cc007a2393ea82cd203b932
Makssell <nimbusbamboo@gmail.com>
2023-04-19 23:19:41 +0200

    fic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth, storage, db } from "../../Projects/Webistes - Html/Chat App/src/firebase";
+import { auth } from "../firebase";
+import logo from "../img/img/logo.png"
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
-    const [err, setErr] = useState(false)
-  const navigate = useNavigate()
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const email = e.target[0].value;
     const password = e.target[1].value;
 
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/")
+    } catch (err) {
+        console.log(err);
+        if (err.code === 'auth/invalid-email') {
+          setErr('Invalid email');
+        } 
+        if (err.code === 'auth/email-already-in-use') {
+          setErr('Email already in use');
+        } 
+        if (err.code === 'auth/missing-email') {
+          setErr('Please fill out all fields');
+        } 
+        if (err.code === 'auth/weak-password') {
+          setErr(' Password should be at least 6 characters');
+        } 
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo"><img src={logo} alt="" /></span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {err && <span>{err}</span>}
+        </form>
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
 
-try {
-    await signInWithEmailAndPassword(auth, email, password);
-    navigate("/")
-}catch(err){
-  console.log(err);
-  setErr(true)
-}
-  }
-
-    return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">ChatNow</span>
-                <span className="title">Login</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder='Your Email'/>
-                    <input type="password" placeholder='Password'/>
-                    <button>Log In</button>
-                    {err && <span>Something went wrong</span>}
-                </form>
-                <p>No account? <Link to="/register">Register</Link></p>
-            </div>
-        </div>
-      )
-}
-
-export default Login
+export default Login;
